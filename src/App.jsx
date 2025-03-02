bf633c7859433cf4d8c6c171d0797b7749d9ae1d
ZunaidzZ <zonaid888@gmail.com>
2025-03-02 14:57:43 +0600

    Initial React project setup with Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <div>
+        <p className='text-3xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit optio quidem reiciendis! Recusandae eum voluptatum neque reiciendis ipsa omnis numquam laborum error repellendus, iste obcaecati rem provident velit voluptates dolorem.</p>
+      </div>
+    </>
+  )
+}
+
+export default App
